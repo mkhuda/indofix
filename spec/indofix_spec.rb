5e06e5e90a4ecbf4e6bf788d615a9b91428195ed
mkhuda <mhmmd.huda@gmail.com>
2018-05-16 20:29:46 +0700

    add more Rspec test

--- spec/indofix_spec.rb
+++ spec/indofix_spec.rb
@@ -14,9 +14,30 @@ RSpec.describe Indofix do
     expect(Indofix.check("other", "keterlaluan")).to be_a_kind_of(Array)
   end
 
-  
+  it "Stupid Check" do
+    expect(Indofix.stupid_check("andaikan")).to be_a_kind_of(Hash)
+  end
+
   it "Nomina Single Result Check" do
     expect(Indofix.check("nomina", "sulapan")).to eq(["sulap"])
   end
+
+  # Indofix::Error Check
+  it "Indofix Error Raise On Check Method without Params" do
+    expect { Indofix.check("", "sulapan") }.to raise_error(Indofix::Error)
+  end
+
+  it "Indofix Error Raise On Check Method without String Params" do
+    expect { Indofix.check("nomina", "") }.to raise_error(Indofix::Error)
+  end
+
+  it "Indofix Error Raise On Check Method with empty params" do
+    expect { Indofix.check("", "") }.to raise_error(Indofix::Error)
+  end
+  
+  it "Indofix Error Raise On Stupid Check Method with empty params" do
+    expect { Indofix.stupid_check("") }.to raise_error(Indofix::Error)
+  end
+
 
 end
